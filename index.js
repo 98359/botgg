bfc8b3e600a75b439ea9f7d107650827da9b086b
98359 <40171217+98359@users.noreply.github.com>
2018-07-03 20:13:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,173 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "f?"
+const prefix = "f."
 
 client.on('ready', () => {
-  client.user.setGame('f?help/f.help|empero-flex.ml ', 'https://www.twitch.tv/lucasdavid913')
+  client.user.setGame('f.help|empero-flex.ml ', 'https://www.twitch.tv/lucasdavid913')
 })   
 
-  client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-
-  if ( message.content.startsWith(prefix+ "say")) {
-    message.delete()
-    const embed = new Discord.RichEmbed()
-    .setDescription(args.join(" "))
-    .setColor('RANDOM')
-   message.channel.sendEmbed(embed);
-  }
-
-});
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-
-  if ( message.content.startsWith(prefix+ "help")) {
-    const embed = new Discord.RichEmbed()
-    .setTitle('BOT INFO')
-    .setDescription(`•prefix --> f?
-
-BOT COMMANDS
-
-• || f?mass || f?support || f?info || f?say || f?ping || f?serverinfo || f?restart || f?avatar || f?purge || f?invite || f?8ball || f?botinfo || `)
-    .setColor('RANDOM')
-   message.channel.sendEmbed(embed);
-  }
-
-});
-
-client.on('message', message => {
-    if (message.author.id == '419472407816830986') {
-    if (message.content.startsWith(`f?restart`)) {
-            resetBot(message.channel);
-    }
-  }
-});
-
-// Turn bot off (destroy), then turn it back on
-function resetBot(channel) {
-    // send channel a message that you're resetting bot [optional]
-    channel.send('Restarting...')
-    .then(msg => bot.destroy())
-    .then(() => bot.login(process.env.TOKEN));
-}
-
  
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-  
-if ( message.content.startsWith(prefix+ "ping")) {
-    let embed = new Discord.RichEmbed()
-            .setTitle('Ping')
-            .setDescription(`\`\`\`md\nPing :${client.ping}ms\`\`\``)
-            .setColor('RANDOM')
-            .setFooter('Bot pinged')
-            .setTimestamp()
-        
-        message.channel.send(embed);
-  }
-  
-});
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-  
-if (message.content.startsWith(prefix + 'serverinfo')) {
-  const embed = new Discord.RichEmbed()
-  embed.addField('Members', message.guild.memberCount, true)
-  embed.addField('Name', message.guild.name, true)
-  embed.addField('Region', message.guild.region, true)
-  embed.addField('Owner', message.guild.owner, true)
-  embed.addField('ID', message.guild.id, true)
-  embed.setColor('RANDOM')
-  embed.setThumbnail(message.guild.iconURL)
-  message.channel.sendEmbed(embed)
-}
-  
-});
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-  
-if (message.content.startsWith(prefix + 'botinfo')) {
-  const embed = new Discord.RichEmbed()
-  embed.addField('BOT INFO')
-  embed.addField('Name', 'Flex', true)
-  embed.addField('Prefix', 'f?', true)
-  embed.addField('Tag:', '6777', true)
-  embed.addField('Created at:', '2018-06-20 12:52:59.660000', true)
-  embed.addField('ID', '458912845438910464', true)
-  embed.addField('Made with:', 'Node')
-  embed.addField('Creator', '<@419472407816830986>', true)
-  embed.setColor('RANDOM')
-  embed.setThumbnail(message.guild.iconURL)
-  message.channel.sendEmbed(embed)
-}
-  
-});
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-  
-if (message.content.startsWith(prefix + 'virus')) {
-  const embed = new Discord.RichEmbed()
-  embed.addField('Virus')
-  embed.addField('loading....', '<a:LOADING:463652782478721024>', true)
-  embed.setColor('RANDOM')
-  embed.setThumbnail(message.guild.iconURL)
-  message.channel.sendEmbed(embed)
-}
-  
-});
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-
-  if ( message.content.startsWith("https://discord.gg")) {
-    message.delete()
-    let embed = new Discord.RichEmbed()
-            .setTitle('No Invite Link')
-            .setColor('RANDOM')
-            .setFooter('No invite link!')
-            .setTimestamp()
-        
-        message.channel.send(embed);
-  }
-
-});  
-   
-  client.on('message', message => {    
-    if(message.content.startsWith('f?mass')) {
-    if(message.author.id === "419472407816830986" ||
-message.author.id === "404708655578218511"){
-       let args = message.content.split(" ").slice(1);         
-       var argresult = args.join(" ")      
-const argsresult = args.join(" ")         
-       let reason = args.join(" ")                   
-    if(!args[1]) {  }  
-    if(args[1]) { 
-message.channel.guild.members.forEach(member => {{ 
-member.send(reason)
-message.delete() }})}}} });
-
-client.on('message', msg => {
-    if (msg.content === 'f?invite') {
-      msg.channel.send('Invite **Flex** https://discordapp.com/oauth2/authorize?client_id=458912845438910464&permissions=8&scope=bot  :tada:');
-    }
-  });
-
-client.on('message', msg => {
-    if (msg.content === 'f?support') {
-      msg.channel.send('support server ---> https://discord.gg/GF3RWsd');
-    }
-  });
-
-client.on('message', msg => {
-    if (msg.content === 'f?info') {
-      msg.channel.send('creatorul meu este : <@419472407816830986> si <@404708655578218511> ');
-    }
-  });
 
 client.on('guildMemberAdd', member => {
     var joinrole = member.guild.roles.find('name', '[F]Member');
@@ -179,7 +18,7 @@ client.on('guildMemberAdd', member => {
         .setColor('#FF000')
         .setThumbnail(memberavatar)
         .addField('✘ | Name: ', `${member}`)
-        .addField('✘ | Welcome', `Bine ai venit <a:flex:458662125771030571> `)
+        .addField('✘ | Welcome', `Bine ai venit <:flex:458964208348823552> `)
         .setTimestamp()
 
         channel.sendEmbed(embed);
